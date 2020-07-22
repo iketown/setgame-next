38eda2f72253a99305c1b5bc7e52bcdf09da5be5
ike76 <ike76@me.com>
2020-07-22 14:26:01 -0500

    fix GameRequestButton error

--- src/components/GamePlayers/GameRequestButton.tsx
+++ src/components/GamePlayers/GameRequestButton.tsx
@@ -1,25 +1,23 @@
+/* eslint-disable consistent-return */
+import { Button, LinearProgress } from "@material-ui/core";
+import { useUserCtx } from "context/user/UserCtx";
 import React, { useEffect, useState } from "react";
+
 import { useGameCtx } from "../../../context/game/GameCtx";
-import { Button, CircularProgress, LinearProgress } from "@material-ui/core";
 import { useGame } from "../../hooks/useGame";
-import { useFBCtx } from "../../../context/firebase/firebaseCtx";
 
 const GameRequestButton = () => {
-  const { user } = useFBCtx();
+  const { user } = useUserCtx();
   const { isPlayer, gameRef } = useGameCtx();
   const { requestToJoin } = useGame();
-  const [waiting, setWaiting] = useState(false);
 
   useEffect(() => {
     if (isPlayer || !gameRef || !user?.uid) return;
     const joinReqRef = gameRef.child("joinRequests");
-    console.log("setting listener");
     joinReqRef.on("value", (snap) => {
       const value = snap.val();
-      console.log("value", value);
       if (value && value[user.uid]) {
         const { requestTime, approved } = value[user.uid];
-        setWaiting(true);
       }
     });
     return () => joinReqRef.off();
