d1861af997bca7d9dbdabac771b0d86c728ba92a
ike76 <ike76@me.com>
2020-07-28 06:58:01 -0500

    remove facebook sign in

--- src/components/SignInScreen.tsx
+++ src/components/SignInScreen.tsx
@@ -15,7 +15,6 @@ const uiConfig = {
   // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
   ],
